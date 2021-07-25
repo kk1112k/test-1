fb487b8bd01423bc650c5951480383029c185edc
kk1112k <87919596+kk1112k@users.noreply.github.com>
2021-07-25 11:33:24 +0900

    inital commit

---
+++ src/sample1/Hello.java
@@ -0,0 +1,15 @@
+package sample1;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		
+	
+		// 2021년 5월 4일 첫번째 프로그램 시작
+		System.out.println("Hello world!! ");
+		/*************************
+		 *  여러줄 주석
+		 ************************/
+}
+
+}
---
+++ src/sample1/Hello2.java
@@ -0,0 +1,25 @@
+package sample1;
+
+public class Hello2 {
+
+	public static void main(String[] args) {
+		
+		int tmp = 5;
+		
+		System.out.println(++tmp);	//전치증가연산자
+		System.out.println(tmp);  	//tmp값 그대로 출력
+		System.out.println(tmp++);	//후치증가연산자
+		System.out.println(tmp);	//tmp값 그대로 출력
+		
+		/****************************************
+		System.out.print(7);
+		System.out.println(3);*되네..");
+		System.out.println("숫자"+3+"과 문자열의 연결도 가능");
+		System.out.println(3+5+"숫자먼저 연산후");
+		System.out.println("문자열먼저 나오고 연산은" + 3+ 5);
+	    *****************************/
+	}
+
+}
+
+
